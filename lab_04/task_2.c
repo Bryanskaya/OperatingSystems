3f7a49557da1c0f28e396fd4c5d084f37e7cab32
Bryanskaya <54107546+Bryanskaya@users.noreply.github.com>
2020-11-14 18:22:49 +0300

    Add files via upload
    ADD: task 2

--- lab_04/task_2.c
+++ lab_04/task_2.c
@@ -1,33 +1,58 @@
 /*
-Написать программу по схеме первого задания, но в процессе-предке выполнить системный вызов wait(). 
-Убедиться, что в этом случае идентификатор процесса потомка на 1 больше идентификатора 
-процесса-предка.
+Написать программу по схеме первого задания, но в процессе-предке выполнить системный вызов wait(). Убедиться, что в этом случае идентификатор процесса потомка на 1 больше идентификатора процесса-предка.
 */
 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	pid_t childpid;
+	pid_t childpid = fork();
+	int status;
 
-	if ((chiltpid = fork()) == -1)
+	if (childpid == -1)
 	{
 		perror("Can't fork");
-		
 		exit(1);
 	}
 	
 	if (childpid == 0)	// Потомок 
 	{
-		//sleep(5);
-		printf("Child: id = %d parent_id = %d group_id = %d ", getpid(), getppid(), getpgrp());
+		printf("Child:  id = %d \tparent_id = %d \tgroup_id = %d\n", getpid(), getppid(), getpgrp());
 	}
-	else (childpid == 1)	// Предок
+	else	// Предок
 	{
-		printf("Parent: id = %d	group_id = %d children = %d\n", getpid(), getpgrp(), ******);
-		wait();
-
-		return 0;
+		printf("Parent: id = %d	group_id  = %d \tchildren = %d\n", getpid(), getpgrp(), childpid);
+		
+		childpid = fork();
+		
+		if (childpid == -1)
+		{
+			perror("Can't fork");
+			exit(1);
+		}
+		
+		if (childpid == 0)	// Потомок 
+		{
+			printf("Child:  id = %d \tparent_id = %d \tgroup_id = %d\n", getpid(), getppid(), getpgrp());
+			return 0;
+		}
+		
+		for (int i = 0; i < 2; i++)
+		{
+			childpid = wait(&status);
+			printf("\nChild finished: pid = %d\n", childpid);
+			
+			if (WIFEXITED(status))
+				printf("Child exited with code %d\n", WEXITSTATUS(status));
+			else printf("Child terminated abnormally\n");
+		}
+		
 	}
-{
+
+	return 0;
+}
+
