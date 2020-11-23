2e045a8d49e1de68d897f44a433905abbeeae500
Bryanskaya <bryanskayakatya@yandex.ru>
2020-11-23 16:32:38 +0300

    CORRECTED: task 5 and report

--- lab_04/task_5.c
+++ lab_04/task_5.c
@@ -22,100 +22,11 @@ void action(int sig_numb)
 		send_signal_2 = 1;				
 }
 
-int child_action_1(int fd[])
-{
-	char send_msg[25];
-	int status;
-
-	printf("Child:  id = %d \tparent_id = %d \tgroup_id = %d\n", getpid(), getppid(), getpgrp());
-		
-	if (close(fd[0]) == -1)
-	{
-		printf("Close error");
-		return -1;
-	}
-	
-	sleep(1);
-	sprintf(send_msg, "Hello, it's child %d", getpid());
-	status = write(fd[1], send_msg, sizeof(send_msg));
-	if (status == -1)
-	{
-		printf("Write error");
-		return -1;
-	}
-	
-	if (close(fd[1]) == -1)
-	{
-		printf("Close error");
-		return -1;
-	}
-	
-	printf("\n- Child %d sent: %s\n", getpid(), send_msg);
-	
-	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)		// Ctrl-'\'
-	{
-		printf("Signal error");
-		return -1;
-	}
-	
-	sleep(5);
-				
-	if (send_signal_1)
-		printf("\nChild %d received signal\n", getpid()); 
-		
-	return 0;
-}
-
-int child_action_2(int fd[])
-{
-	char send_msg[25];
-	int status;
-	
-	printf("Child:  id = %d \tparent_id = %d \tgroup_id = %d\n", getpid(), getppid(), getpgrp());
-	
-	if (close(fd[0]) == -1)
-	{
-		printf("Close error");
-		return -1;
-	}
-	
-	sleep(1);
-	sprintf(send_msg, "Hello, it's child %d", getpid());
-	status = write(fd[1], send_msg, sizeof(send_msg));
-	if (status == -1)
-	{
-		printf("Write error");
-		return -1;
-	}
-	
-	if (close(fd[1]) == -1)
-	{
-		printf("Close error");
-		return -1;
-	}
-	
-	printf("\n- Child %d sent: %s\n", getpid(), send_msg);
-	
-	if (signal(SIGTSTP, SIG_IGN) == SIG_ERR)		// Ctrl-z
-	{
-		printf("Signal error");
-		return -1;
-	}
-	
-	sleep(5);
-	
-	if (send_signal_2)
-		printf("\nChild %d received signal\n", getpid());
-		
-	return 0;
-}
-
-
 int main(int argc, char *argv[])
 {
 	int fd[2];
 	int status;
-	char get_msg[25];
+	char get_msg[25], send_msg[25];
 	pid_t childpid_1, childpid_2;
 	
 	if (signal(SIGTSTP, action) == SIG_ERR || signal(SIGQUIT, action) == SIG_ERR)    // Ctrl-z и Ctrl-'\'
@@ -140,10 +51,41 @@ int main(int argc, char *argv[])
 	
 	if (childpid_1 == 0)
 	{
-		status = child_action_1(fd);
-		if (status)
+		printf("Child:  id = %d \tparent_id = %d \tgroup_id = %d\n", getpid(), getppid(), getpgrp());
+		
+		if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)		// Ctrl-'\'
+		{
+			printf("Signal error");
 			exit(1);
+		}
 			
+		if (close(fd[0]) == -1)
+		{
+			printf("Close error");
+			exit(1);
+		}
+	
+		sleep(5);
+		
+		if (send_signal_1)
+		{
+			sprintf(send_msg, "Hello, it's child %d", getpid());
+			status = write(fd[1], send_msg, sizeof(send_msg));
+			if (status == -1)
+			{
+				printf("Write error");
+				exit(1);
+			}
+			
+			printf("\n- Child %d sent: %s\n", getpid(), send_msg);
+		}
+		
+		if (close(fd[1]) == -1)
+		{
+			printf("Close error");
+			exit(1);
+		}
+	
 		return 0;
 	}
 
@@ -159,9 +101,40 @@ int main(int argc, char *argv[])
 	
 	if (childpid_2 == 0)
 	{
-		status = child_action_2(fd);
-		if (status)
+		printf("Child:  id = %d \tparent_id = %d \tgroup_id = %d\n", getpid(), getppid(), getpgrp());
+		
+		if (signal(SIGTSTP, SIG_IGN) == SIG_ERR)		// Ctrl-'\'
+		{
+			printf("Signal error");
 			exit(1);
+		}
+			
+		if (close(fd[0]) == -1)
+		{
+			printf("Close error");
+			exit(1);
+		}
+		
+		sleep(5);
+		
+		if (send_signal_2)
+		{
+			sprintf(send_msg, "Hello, it's child %d", getpid());
+			status = write(fd[1], send_msg, sizeof(send_msg));
+			if (status == -1)
+			{
+				printf("Write error");
+				exit(1);
+			}
+			
+			printf("\n- Child %d sent: %s\n", getpid(), send_msg);
+		}
+		
+		if (close(fd[1]) == -1)
+		{
+			printf("Close error");
+			exit(1);
+		}
 
 		return 0;
 	}
@@ -199,23 +172,29 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	
-	status = read(fd[0], get_msg, sizeof(get_msg));
-	if (status == -1)
+	if (send_signal_1)
 	{
-		printf("Read 1 error");
-		exit(1);
+		status = read(fd[0], get_msg, sizeof(get_msg));
+		if (status == -1)
+		{
+			printf("Read 1 error");
+			exit(1);
+		}
+		
+		printf("\n--> Parent read: %s\n", get_msg);
 	}
 	
-	printf("\n--> Parent read: %s\n", get_msg);
-	
-	status = read(fd[0], get_msg, sizeof(get_msg));
-	if (status == -1)
+	if (send_signal_2)
 	{
-		printf("Read 2 error");
-		exit(1);
+		status = read(fd[0], get_msg, sizeof(get_msg));
+		if (status == -1)
+		{
+			printf("Read 2 error");
+			exit(1);
+		}
+		
+		printf("\n--> Parent read: %s\n", get_msg);
 	}
-	
-	printf("\n--> Parent read: %s\n", get_msg);
 	
 	if (close(fd[0]) == -1)
 	{
