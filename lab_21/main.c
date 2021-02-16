aea07f03c327c321840a7ba08fce113ff6263412
Bryanskaya <bryanskayakatya@yandex.ru>
2021-02-17 00:49:21 +0300

    ADD: code

--- lab_21/main.c
+++ lab_21/main.c
@@ -10,11 +10,15 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/resource.h>
+#include <sys/file.h>
 #include <pthread.h>
+#include <signal.h>
+#include <time.h>
 
 #define LOCKFILE "/var/run/daemon.pid"
 #define LOCKMODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) // права достипа: пользователь на чтение, запись; группа на чтение; остальные на чтение
 
+sigset_t mask;
 
 int lockfile(int fd)
 {
@@ -59,11 +63,13 @@ int already_running()
     sprintf(buf, "%ld", (long)getpid()); // запись идентификатора демона в файл
     write(fd, buf, strlen(buf) + 1);
 
-    return 0;
+    return(0);
 }
 
 void daemonize(const char *cmd)
 {
+    int fd0, fd1, fd2;
+    pid_t pid;
     struct rlimit rl;
     struct sigaction sa;
 
@@ -71,13 +77,16 @@ void daemonize(const char *cmd)
     umask(0);   // сбросить маску режима создания файлов
 
     if (getrlimit(RLIMIT_NOFILE, &rl) == -1) // получить макимально возможный номер дескриптора,  максимальное число файлов, которые могут быть одновременно открыты процессом
-        perror("ERROR: impossible to get max number of the descriptor");
+        perror("ERROR: impossible to get max number of the descriptor\n");
 
     // стать лидером новой сессии, чтобы утратить управляющий терминал 
     if ((pid = fork()) == -1)
-        perror("ERROR: fork()");
+        perror("ERROR: fork()\n");
     else if (pid != 0)  // родительский процесс
+    {
+        printf("***** Child PID %d *****\n", pid);
         exit(0);
+    }
 
     setsid();   // создать новую сессию
 
@@ -121,16 +130,45 @@ void daemonize(const char *cmd)
                                            LOG_DAEMON - сообщения от других демонов системы*/
     if (fd0 != 0 || fd1 != 1 || fd2 != 2)
     {
-        syslog(LOG_ERR, "ERROR: mistake in file's descriptors %d %d %d\n", fd0, fd1, fd2);
+        syslog(LOG_ERR, "ERROR: mistake in file's descriptors %d %d %d", fd0, fd1, fd2);
         exit(1);
     } 
 } 
 
+void* thr_fn(void *arg)
+{
+    int err, signo;
+    time_t scnd = time(NULL);
 
-int main()
+    for (;;)
+    {
+        err = sigwait(&mask, &signo);
+        if (err)
+        {
+            syslog(LOG_ERR, "ERROR: mistake in call function sigwait");
+            exit(1);
+        }
+
+        switch (signo)
+        {
+        case SIGHUP:
+            syslog(LOG_INFO, ">>> INFO: USER'S NAME: %s    TIME: %s", getlogin(), asctime(localtime(&scnd)));
+            break;
+        case SIGTERM:
+            syslog(LOG_INFO, "INFO: received signal SIGTERM. EXIT");
+            exit(0);
+        default:
+            syslog(LOG_INFO, "INFO: received unexpected signal");
+        }
+    }
+
+    return(0);
+}
+
+
+int main(int argc, char *argv[])
 {
     char *cmd;
-    sigset_t mask;
     int err;
     pthread_t tid;
     struct sigaction sa;
@@ -149,7 +187,36 @@ int main()
         exit(1);
     }
 
+    sa.sa_handler = SIG_DFL;    // выполнение действий по умолчанию - в большинстве случаев - окончание процесса.
+    sigemptyset(&sa.sa_mask); // установка маски сигналов
+    sa.sa_flags = 0;
 
+    if (sigaction(SIGHUP, &sa, NULL) == -1)
+    {
+        syslog(LOG_ERR, "ERROR: mistake in recovering SIG_DFL for SIGHUP");
+        exit(1);
+    }
+
+
+    sigfillset(&mask);  //полностью инициализирует набор set, в котором содержатся все сигналы
+
+    if (pthread_sigmask(SIG_BLOCK, &mask, NULL))    // добавьте набор сигналов к набору заблокированных сигналов
+    {
+        syslog(LOG_ERR, "ERROR: mistake with SIG_BLOCK");
+        exit(1);
+    }
+
+    if (pthread_create(&tid, NULL, thr_fn, 0))
+    {
+        syslog(LOG_ERR, "ERROR: mistake in creating thread");
+        exit(1);
+    }
+
+    if (pthread_join(tid, NULL))
+    {
+        syslog(LOG_ERR, "ERROR: mistake in joining thread");
+        exit(1);
+    }
 
     exit(0);
 }
