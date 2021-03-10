c09257297f30e15e28b64385e94194be926f928e
Bryanskaya <bryanskayakatya@yandex.ru>
2021-03-10 23:35:54 +0300

    CHECKED: work with softlink

--- lab_22/main.c
+++ lab_22/main.c
@@ -16,54 +16,6 @@
 #define FTW_NS  4       
 
 
-typedef int Myfunc(const char *, int);
-
-
-static int dopath(const char *filename, Myfunc *func, int n) 
-{
-    struct stat     statbuf;    
-    struct dirent   *dirp;      
-    DIR             *dp;         
-    int             ret;    
-
-
-    if (lstat(filename, &statbuf) < 0)
-        return(func(filename, FTW_NS));
-
-    for (int i = 0; i < n; i++)
-        printf("______");
-
-    if (S_ISDIR(statbuf.st_mode) == 0)  
-        return(func(filename, FTW_F));
-
-
-    func(filename, FTW_D);
-
-    if ((dp = opendir(filename)) == NULL)
-        return(func(filename, FTW_DNR));
-
-    chdir(filename);
-
-    while (dirp = readdir(dp))
-    {
-        if (strcmp(dirp->d_name, ".") == 0 ||       
-            strcmp(dirp->d_name, "..") == 0)        
-            continue; 
-
-        if ((ret = dopath(dirp->d_name, func, n + 1)))   
-            break;
-    }
-
-    chdir("..");
-
-    if (closedir(dp) < 0) 
-        printf("ERROR: imposiible to close catalog %s", filename);
-
-    return(ret);
-}
-
-
-
 static int showFiles(const char *pathname, int type)
 {
     switch (type)
@@ -89,6 +41,47 @@ static int showFiles(const char *pathname, int type)
     return 0;
 }
 
+
+static int dopath(const char *filename, int n) 
+{
+    struct stat     statbuf;    
+    struct dirent   *dirp;      
+    DIR             *dp;         
+    int             ret = 0;    
+
+
+    if (lstat(filename, &statbuf) < 0)
+        return(showFiles(filename, FTW_NS));
+
+    for (int i = 0; i < n; i++)
+        printf("______");
+
+    if (S_ISDIR(statbuf.st_mode) == 0)  
+        return(showFiles(filename, FTW_F));
+
+
+    showFiles(filename, FTW_D);
+
+    if ((dp = opendir(filename)) == NULL)
+        return(showFiles(filename, FTW_DNR));
+
+    chdir(filename);
+
+    while ((dirp = readdir(dp)) != NULL && (ret == 0))
+    {
+        if (strcmp(dirp->d_name, ".") && strcmp(dirp->d_name, ".."))       
+            ret = dopath(dirp->d_name, n + 1);
+    }
+
+    chdir("..");
+
+    if (closedir(dp) < 0) 
+        printf("ERROR: imposiible to close catalog %s", filename);
+
+    return(ret);
+}
+
+
 int main(int argc, char *argv[])
 {
     int ret;
@@ -99,7 +92,7 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    ret = dopath(argv[1], showFiles, 0);
+    ret = dopath(argv[1], 0);
     
     return ret;
 }
