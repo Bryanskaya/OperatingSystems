bf15bbff9f20e89da525891729451b9927ebc31a
Bryanskaya <bryanskayakatya@yandex.ru>
2021-03-27 18:07:04 +0300

    STARTED: part 2

--- lab_24/2_part/main.c
+++ lab_24/2_part/main.c
@@ -1,46 +1,114 @@
 #include <linux/vmalloc.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/vmalloc.h>
-
-#include <init.h>
+#include <linux/proc_fs.h>
 #include <linux/string.h>
 
+#include <linux/uaccess.h>
+#include <asm/uaccess.h>
+
+
+#define MAX_COOKIE_LENGTH   PAGE_SIZE
+#define PROC_FILE_NAME      "my_fortune"
 
 MODULE_LICENSE("GPL");
 
 
-static struct proc_dir_entry *proc_entry;
+static struct proc_dir_entry *proc_file;
+/*static struct proc_dir_entry *proc_slink;
+static struct proc_dir_entry *proc_dir;*/
 
 
-char* cookie_pot;
+static char* cookie_pot;    // хранилище фортунок
+static int cookie_index, next_fortune;  /* индекс первого свободного для записи элемента хранилища
+                                           индекс элемента хранилища, содержащего следующую фортунку для вывода по запросу*/
 
-static int init_fortune_module()
+
+ssize_t fortune_write(struct file *file, const char __user *buff, unsigned long len, loff_t *)
+{
+    int free_space = (MAX_COOKIE_LENGTH - cookie_index) + 1;
+
+    if (free_space < len)   // достаточно ли места для размещения фортунки
+    {
+        printk(KERN_ERR "my_fortune: cookie_pot is overflowed");
+        return -EFAULT;
+    }
+
+    if (copy_from_user(&cookie_pot[cookie_index], buff, len)) // если место есть, то строка копируется в cookie_pot
+    {
+        printk(KERN_INFO "my_fortune: copy_from_user error");
+        return -EFAULT;
+    }
+    
+    cookie_index += len;
+    cookie_pot[cookie_index - 1] = 0;
+
+    return len;     // количество символов фактически записанных в cookie_pot
+}
+
+int fortune_read(char *page, char **start, off_t off, int count, void *data)
+{
+    int len;
+
+    if (!cookie_index || off > 0) // какой-то *off > 0   || )
+        return 0;
+
+    if (next_fortune >= cookie_index)
+        next_fortune = 0;
+
+    len = sprintf(page, "%s\n", &cookie_pot[next_fortune]);
+
+    next_fortune += len;
+
+    return len;
+}
+
+
+static const struct file_operations fops = 
+{
+    .owner = THIS_MODULE,
+    .read = fortune_read,
+    .write = fortune_write,
+};
+
+
+
+static int __init init_fortune_module(void)
 {
     cookie_pot = (char*)vmalloc(MAX_COOKIE_LENGTH);
     if (!cookie_pot)
     {
-        printk(KERN_INFO "fortune: impossible to malloc cookie_pot");
+        printk(KERN_INFO "my_fortune: impossible to malloc cookie_pot");
         return -ENOMEM;
     }
 
     memset(cookie_pot, 0, MAX_COOKIE_LENGTH);
-    proc_entry = proc_create(?????????);
 
-    if (!proc_entry)
+    proc_file = proc_create(PROC_FILE_NAME, 0666, NULL, &fops); //////
+
+    if (!proc_file)
     {
         vfree(cookie_pot);
-        printk(KERN_INFO "fortune: can't create proc entry");
+        printk(KERN_INFO "my_fortune: can't create proc entry");
         return -ENOMEM;
     }
+
+    cookie_index = 0;
+    next_fortune = 0;
 
     printk(KERN_INFO "===== Fortune module loaded! =====");
 
     return 0;
 }
 
-static void exit_fortune_module()
+static void __exit exit_fortune_module(void)
 {
+    if (proc_file)
+        remove_proc_entry(PROC_FILE_NAME, NULL);
+
+    if (cookie_pot)
+        vfree(cookie_pot);
+
     printk(KERN_INFO "===== Fortune module unloaded! =====");
 }
 
