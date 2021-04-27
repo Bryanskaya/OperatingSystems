5370057136e20c7c60db9e463d40ce85ec2f3711
Bryanskaya <bryanskayakatya@yandex.ru>
2021-04-27 23:54:17 +0300

    STARTED: to do fortune

---
+++ lab_24/add_task_seq_file/main.c
@@ -0,0 +1,162 @@
+#include <linux/vmalloc.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/string.h>
+
+#include <linux/uaccess.h>
+#include <asm/uaccess.h>
+
+#include <seq_file.h>
+
+
+#define MAX_COOKIE_LENGTH   PAGE_SIZE
+#define PROC_FILE_NAME      "my_fortune"
+#define PROC_FILE_NAME_2    "my_fortune_2"
+#define PROC_DIR_NAME       "my_dir"
+#define PROC_SLINK_NAME     "my_slink"
+
+MODULE_LICENSE("GPL");
+
+
+static struct proc_dir_entry *proc_file;
+static struct proc_dir_entry *proc_file_2;
+static struct proc_dir_entry *proc_slink;
+static struct proc_dir_entry *proc_dir;
+
+
+static char* cookie_pot;
+static int cookie_index, next_fortune;
+
+
+static ssize_t fortune_write(struct file *filp, const char __user *buff, size_t max_bytes_to_write, loff_t *f_pos)
+{
+    int free_space = (MAX_COOKIE_LENGTH - cookie_index) + 1;
+
+    if (free_space < max_bytes_to_write)
+    {
+        printk(KERN_ERR "my_fortune: cookie_pot is overflowed");
+        return -EFAULT;
+    }
+
+    if (copy_from_user(&cookie_pot[cookie_index], buff, max_bytes_to_write))
+    {
+        printk(KERN_INFO "my_fortune: copy_from_user error");
+        return -EFAULT;
+    }
+    
+    cookie_index += count;
+    cookie_pot[cookie_index - 1] = '\n';
+
+    cookie_pot[cookie_index] = 0;
+    cookie_index += 1;
+
+    return count;
+}
+
+
+/*ssize_t fortune_read(struct file *filp, char __user *buff, size_t count, loff_t *f_pos)
+{
+    int len;
+
+    if (!cookie_index || *f_pos > 0)
+        return 0;
+
+    if (next_fortune >= cookie_index)
+        next_fortune = 0; 
+
+    len = strlen(&cookie_pot[next_fortune]);
+    copy_to_user(buff, &cookie_pot[next_fortune], len);
+
+    next_fortune += len + 1;
+    *f_pos += len;
+
+    return len;
+}*/
+
+static int proc_show(struct seq_file *m, void *v)
+{
+    return seq_printf(m, "%s\n", cookie_pot);
+}
+
+int simple_proc_open(struct inode *ind, struct file *filp)
+{
+    printk(KERN_INFO "***** Called OPEN *****");
+
+    return single_open(filp, proc_show, NULL);
+}
+
+int simple_proc_release()
+{
+    
+}
+
+
+static struct file_operations fops = 
+{
+    .owner = THIS_MODULE,
+    .open = proc_open,
+    .read = fortune_read,
+    .release = proc_release,
+    .show = proc_show,
+    .write = fortune_write,
+};
+
+
+static int __init init_fortune_module(void)
+{
+    cookie_pot = vmalloc(MAX_COOKIE_LENGTH);
+    if (!cookie_pot)
+    {
+        printk(KERN_INFO "my_fortune: impossible to malloc cookie_pot");
+        return -ENOMEM;
+    }
+
+    memset(cookie_pot, 0, MAX_COOKIE_LENGTH);
+
+    proc_file = proc_create(PROC_FILE_NAME, 0666, NULL, &fops);
+    if (!proc_file)
+    {
+        vfree(cookie_pot);
+        printk(KERN_INFO "my_fortune: proc_create error");
+        return -ENOMEM;
+    }
+
+    proc_file_2 = proc_create(PROC_FILE_NAME_2, 0666, NULL, &fops);
+
+    proc_dir = proc_mkdir(PROC_DIR_NAME, NULL);
+    proc_slink = proc_symlink(PROC_SLINK_NAME, NULL, PROC_FILE_NAME);
+
+    if (!proc_dir || !proc_slink)
+    {
+        vfree(cookie_pot);
+        printk(KERN_INFO "my_fortune: proc_mkdir/proc_slink error");
+        return -ENOMEM;
+    }
+
+    cookie_index = 0;
+    next_fortune = 0;
+
+    printk(KERN_INFO "===== Fortune module loaded! =====");
+
+    return 0;
+}
+
+static void __exit exit_fortune_module(void)
+{
+    remove_proc_entry(PROC_FILE_NAME, NULL);
+    remove_proc_entry(PROC_FILE_NAME_2, NULL);
+    remove_proc_entry(PROC_DIR_NAME, NULL);
+    remove_proc_entry(PROC_SLINK_NAME, NULL);
+
+    if (cookie_pot)
+        vfree(cookie_pot);
+
+    printk(KERN_INFO "===== Fortune module unloaded! =====");
+}
+
+
+
+module_init(init_fortune_module);
+module_exit(exit_fortune_module);
